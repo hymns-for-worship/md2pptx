3479a0125b889101c343f4c7f49fd96d8e0e0946
Rody Davis <rody.davis.jr@gmail.com>
2022-09-06 21:53:20 -0700

    removing test file

--- test/index.test.ts
+++ test/index.test.ts
@@ -13,9 +13,6 @@ it('check if title is added and correct master is used', async () => {
 
     const { _slides } = Object(pres) as any;
     const slide = _slides[0] as pptxgen.Slide;
-    console.log(JSON.stringify(slide, null, 2));
-
-    await pres.writeFile({ fileName: './test.pptx' });
 
     expect(slide.background.color).toBe('FFFFFF');
 });
