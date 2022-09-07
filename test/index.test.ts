319207ce7786b94769fe848b62b2c195a475ee56
Rody Davis <rody.davis.jr@gmail.com>
2022-09-06 21:51:55 -0700

    adding test

--- test/index.test.ts
+++ test/index.test.ts
@@ -12,9 +12,10 @@ it('check if title is added and correct master is used', async () => {
     const pres = generateSlides(markdown);
 
     const { _slides } = Object(pres) as any;
-    const { _master } = Object(_slides[0]) as any;
+    const slide = _slides[0] as pptxgen.Slide;
+    console.log(JSON.stringify(slide, null, 2));
 
     await pres.writeFile({ fileName: './test.pptx' });
 
-    expect(_master).toBe('TITLE_SLIDE');
+    expect(slide.background.color).toBe('FFFFFF');
 });
